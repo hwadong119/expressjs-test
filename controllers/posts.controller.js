ed111dffd73923846fa2f6caeaf24244ef34086e
hwadong119 <jeonghwayun0119@naver.com>
2023-10-03 20:33:41 +0900

    Template Layout 생성

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 function getPost(req,res) {
-  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'forest.jpeg'))
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'forest.jpeg'))
+  res.render('posts', {
+    templateName: 'post'
+  })
 }
 
 module.exports = {
