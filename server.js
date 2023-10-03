c67cede1db5e4f6dcc05e092541e65254dc5df91
hwadong119 <jeonghwayun0119@naver.com>
2023-10-03 19:52:42 +0900

    MVC 패턴으로 변경

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const express = require('express');
+const usersController = require('./controllers/users.controller');
+const postsController = require('./controllers/posts.controller');
 
 const PORT = 4000;
-
-const Users = [
-  {
-    id: 0,
-    name: 'Jack'
-  },
-  {
-    id: 1,
-    name: 'Jennifer'
-  }
-]
 
 const app = express();
 
@@ -25,37 +16,12 @@ app.use((req, res, next) => {
   console.log(`end: ${req.method} ${req.url} ${diffTime}ms`);
 })
 
-app.get('/users', (req, res) => {
-  res.send(Users);
-})
+app.get('/users', usersController.getUsers);
+app.get('/users/:userId', usersController.getUser);
+app.post('/users', usersController.postUser);
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-})
+app.get('/posts', postsController.getPost);
 
-app.post('/users', (req, res) => {
-  if(!req.body.name) {
-    return res.status(400).json({
-      error: 'Missing user name'
-    })
-  }
-  const newUser = {
-    name: req.body.name,
-    id: Users.length
-  }
-  Users.push(newUser);
-  res.json(newUser);
-})
-
-app.get('/users/:userId', (req, res) => {
-  const userId = Number(req.params.userId);
-  const user = Users[userId];
-  if(user) {
-    res.send(user);
-  } else {
-    res.sendStatus(404);
-  }
-})
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
