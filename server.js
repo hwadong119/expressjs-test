36a67e1edb7bdb117844a6acc71ccec0f380f3e8
hwadong119 <jeonghwayun0119@naver.com>
2023-10-03 19:34:10 +0900

    POST 요청하기_express.json()

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const Users = [
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   const start = Date.now();
   console.log(`start: ${req.method} ${req.url}`);
@@ -29,6 +31,20 @@ app.get('/users', (req, res) => {
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+})
+
+app.post('/users', (req, res) => {
+  if(!req.body.name) {
+    return res.status(400).json({
+      error: 'Missing user name'
+    })
+  }
+  const newUser = {
+    name: req.body.name,
+    id: Users.length
+  }
+  Users.push(newUser);
+  res.json(newUser);
 })
 
 app.get('/users/:userId', (req, res) => {
