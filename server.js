3285ca082e03d92457224b4260b28c57a411d796
hwadong119 <jeonghwayun0119@naver.com>
2023-10-03 17:35:18 +0900

    req.params

--- server.js
+++ server.js
@@ -2,10 +2,35 @@ const express = require('express');
 
 const PORT = 4000;
 
+const Users = [
+  {
+    id: 0,
+    name: 'Jack'
+  },
+  {
+    id: 1,
+    name: 'Jennifer'
+  }
+]
+
 const app = express();
+
+app.get('/users', (req, res) => {
+  res.send(Users);
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+})
+
+app.get('/users/:userId', (req, res) => {
+  const userId = Number(req.params.userId);
+  const user = Users[userId];
+  if(user) {
+    res.send(user);
+  } else {
+    res.sendStatus(404);
+  }
 })
 
 app.listen(PORT, () => {
